f28f6342d33936c46a8e60dffc84f1db0753b011
Van Russell <77253779+wg481@users.noreply.github.com>
2021-08-30 14:17:05 -0700

    Update Terminal.py

--- Terminal.py
+++ Terminal.py
@@ -7,8 +7,8 @@ import os
 import sys
 import msvcrt
 from getpass import getpass
-#Cool arse terminal loading screen.
-print "Welcome to Vanva Terminal v.2.5 Insider Update."
+#Unneeded loading screen. Intended to simulate actual loading.
+print "Welcome to Vanva Terminal v.1.0b"
 print "Unpacking files..."
 print ''
 time.sleep(2)
@@ -20,14 +20,15 @@ print ''
 print "Please wait while Vanva loads..."
 time.sleep(3)
 print ''
-print "++++++++++++++++++++++++++++"
-print "+ \    /  _____            +"
-print "+  \  /     |              +"
-print "+   \/ anva |erminal v.2.5 +"
-print "++++++++++++++++++++++++++++"
+print "+++++++++++++++++++++++++++++"
+print "+ \    /  _____             +"
+print "+  \  /     |               +"
+print "+   \/ anva |erminal v.1.0b +"
+print "+++++++++++++++++++++++++++++"
 print''
-#This is the savefile.txt activation method.
-#I'm not fully sure that this works, so it's a shot in the dark at best.
+# This ungodly mixture of code you see here is an attempt at savefile.txt activation. Basically, a product key activation system designed to create a savefile.
+# The biggest issue is that the activation could always be fudged via savefile.txt having a 1 written to it by itself, so I'm deciphering other methods like
+# saving a string of numbers unique to the product itself. I wish myself good luck with that.
 def main():
     file_path = "savefile.txt"
     if check_activation(file_path):
@@ -92,13 +93,13 @@ def terminal():
         print "An example of how to use open: open *click enter* c:/program files/VXZLTD/terminal/terminal.exe"
         print "Make sure you know the file path, make it lowercase, and use / instead of \, ok?"
     elif input1 == "ver info":
-        print "~~~Vanva Terminal v.2.0 Insider Update~~~"
+        print "~~~Vanva Terminal v.1.0b Github Update~~~"
         print "Created in Notepad++, a free source code editor and Notepad replacement that supports several languages."
         print "Coded in Python 2.7."
         print "Coded, Edited, and Tested by Van M. Russell."
         print "Installer compiled to .EXE by NSIS and zip2exe."
         print ".PY file Compiled to .EXE by auto-py-to-exe."
-        print "Your Insider build was given to you as a beta program. This version still"
+        print "Your build was developed as part of a program. This version still"
         print "is incompleted and requires you, the tester, to give support and rating"
         print "to determine how to better the program for the future. Insider testing"
         print "reports should be sent via email to vanrussel23@gmail.com. We thank you"
@@ -112,15 +113,15 @@ def terminal():
         text1.pack()
     #VanvaX. The developer process.
     elif input1 == "VanvaX":
-        R3sting = ''
+        usernameX = ''
         print "Username: "
         while True:
             y = msvcrt.getch()
             if y == '\r':
                 break;
             sys.stdout.write('~')
-            R3sting +=y
-        Nincompoopy = ''
+            usernameX +=y
+        passwordX = ''
         print ''
         print 'Password: '
         while True:
@@ -128,8 +129,8 @@ def terminal():
             if x == '\r':
                 break;
             sys.stdout.write('~')
-            Nincompoopy +=x
-        if Nincompoopy == "Gypsy" and R3sting == "Developer":
+            passwordX +=x
+        if passwordX == "Gypsy" and usernameX == "Developer":
             print ''
             print "Welcome to VanvaX Beta v.0.5"
             while True:
